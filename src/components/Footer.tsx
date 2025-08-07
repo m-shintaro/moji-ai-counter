2e3ab2b9fc716bb815ddc917f024f66ce3c21047
Shintaro Matsumoto <s-matsumoto1009@outlook.jp>
2025-08-07 23:29:38 +0900

    fix: update footer link to point to the correct GitHub profile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ const Footer = () => {
     <footer className="bg-github-dark-bg-secondary border-t border-github-dark-border py-4">
       <div className="max-w-6xl mx-auto px-6 text-center">
         <p className="text-github-dark-text-secondary text-sm">
-          Created by{' '}
+          Created by{" "}
           <a
-            href="https://github.com/xyzmiku"
+            href="https://m-shintaro.github.io/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-github-dark-accent hover:underline"
@@ -15,7 +15,7 @@ const Footer = () => {
         </p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
